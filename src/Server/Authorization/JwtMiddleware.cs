6f6334fb4e6ab78fd89065efe44294bb6d78479c
Matt Gueit <matthew.g@vendorpanel.com>
2022-02-05 02:42:24 +1100

    Implement the Invoke method for the JwtMiddleware.

--- src/Server/Authorization/JwtMiddleware.cs
+++ src/Server/Authorization/JwtMiddleware.cs
@@ -1,4 +1,6 @@
-﻿namespace FootyTipping.Server.Authorization
+﻿using FootyTipping.Server.Services;
+
+namespace FootyTipping.Server.Authorization
 {
     public class JwtMiddleware
     {
@@ -9,8 +11,17 @@
             _next = next;
         }
 
-        public async Task Invoke(HttpContext context, IJwtUtilities jwtUtilities)
+        public async Task Invoke(HttpContext context, IUserService userService, IJwtUtilities jwtUtilities)
         {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var userId = jwtUtilities.ValidateToken(token);
+            if (userId != null)
+            {
+                // Attach user to context on successful jwt validation
+                context.Items["User"] = userService.GetById(userId.Value);
+            }
+
+            await _next(context);
         }
     }
 }
--- src/Server/Controllers/UsersController.cs
+++ src/Server/Controllers/UsersController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FootyTipping.Server.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class UsersController : ControllerBase
